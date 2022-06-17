a706b8c24078254b44bb6754b1ebac4e5f031bb3
Mustakim-Khan <mustakimzidan@gmail.com>
2022-06-17 15:06:14 +0200

    Clipboard api

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 function pressButtonTest() {
-    console.log('console');
+    //console.log(navigator.clipboard);
+    navigator.clipboard.writeText('Test')
+        .then(() => {
+            console.log('a été écrit')
+        })
+        .catch(() => {
+            console.log("échoué")
+        })
 }
+function printClipboard() {
+    navigator.permissions.query({ name: "clipboard-read" }).then((result) => {
+        if (result.state === "granted" || result.state === "prompt") {
+            console.log("Read access ranted!");
+            navigator.clipboard.readText()
+                .then((copiedText) => {
+                    console.log(copiedText);
+                })
+        }
+    });
+}
+
+function copyToClipboard() {
+    let copyText = document.getElementById("content").value;
+    navigator.clipboard.writeText(copyText).then(() => {
+        // Alert the user that the action took place.
+        // Nobody likes hidden stuff being done under the hood!
+        console.log("Copied to clipboard");
+    });
+}
+document.addEventListener('copy', (e) => {
+
+});
