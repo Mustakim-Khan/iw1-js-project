378a8584e24a79ef9ddf3dcf426886d0c2cb202e
Mustakim-Khan <mustakimzidan@gmail.com>
2022-06-22 07:40:06 +0200

    Rebase mkhan/kanban from develop

--- www/app.js
+++ www/app.js
@@ -269,3 +269,46 @@ window.addEventListener('pathnamechange', () => {
 
     }
 });
+
+
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', (e) => {
+        draggable.classList.add('dragging')
+    });
+
+    draggable.addEventListener('dragend', (e) => {
+        console.log('start');
+        draggable.classList.remove('dragging')
+    });
+})
+
+
+containers.forEach(container => {
+    container.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(container, e.clientY);
+        const dragged = document.querySelector('.dragging');
+
+        if (afterElement == null) {
+            container.appendChild(dragged);
+        } else {
+            container.insertBefore(dragged, afterElement);
+        }
+    });
+})
+
+
+const getDragAfterElement = (container, y) => {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragged)')];
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset : offset, element : child }
+        }
+        return closest;
+    }, { offset : Number.NEGATIVE_INFINITY }).element;
+}
